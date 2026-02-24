f0612d6f5297f3cf3454245af8cdf8e75f24bd49
Samoney2005 <samone.a.cook-1@ou.edu>
2026-02-24 15:59:14 -0600

    new changes

---
+++ 02-24-2026_tictactoe1.py
@@ -0,0 +1,24 @@
+# TIC TAC TOE GAME
+# Steps:
+# Step 1: initialize all the lists as empty
+# Step 2: List will be needed with 9 numbers
+# - Step 3: Ask the user if they want to play tic-tac-toe
+# - If yes continue to the rest of code, if no exit the game
+# - Step 4: randomize whoever starts either CPU or user
+# - Step 5: pick a letter either x or o
+# -There are 9 places, so user and CPU have to choose a space
+# - Step 6: prompt where the user wants to place their choice between 1 and 9
+# # - Step 7: Check If a spot is free (if not it should have been stored)
+# # - Step 8:  Check if there is a winner (function)
+#
+# spaces = [" "," ", " ", " ", " ", " ", " ", " ", " " ]
+# play = input("Would you like to play tic tac toe?")
+# random_order= random.choice(player)
+# print("This player will start first", random_order)
+#  while True:
+#     if play == "yes":
+#         print("continue")
+#     else:
+#         break
+#
+#
